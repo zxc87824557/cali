fb0de69a00b500f5975ef8c6439fc7f0d426e7ec
Y <Y>
2021-04-07 15:42:30 +0800

    卡利首頁

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-cali
+123ts
+https://karta1252839.github.io/123ts/index.html
+派大金2.0
---
+++ js/indexList.js
@@ -0,0 +1,87 @@
+        $('.AnimaTion1').mousemove(
+            function () {
+                $('.AnimaTion1').attr('src', './卡-img/桌機index_img/icon1-1.png')
+            }
+        )
+        $('.AnimaTion1').mouseout(
+            function () {
+                $('.AnimaTion1').attr('src', './卡-img/桌機index_img/icon1.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion2').mousemove(
+            function () {
+                $('.AnimaTion2').attr('src', './卡-img/桌機index_img/icon2-1.png')
+            }
+        )
+        $('.AnimaTion2').mouseout(
+            function () {
+                $('.AnimaTion2').attr('src', './卡-img/桌機index_img/icon2.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion3').mousemove(
+            function () {
+                $('.AnimaTion3').attr('src', './卡-img/桌機index_img/icon3-1.png')
+            }
+        )
+        $('.AnimaTion3').mouseout(
+            function () {
+                $('.AnimaTion3').attr('src', './卡-img/桌機index_img/icon3.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion4').mousemove(
+            function () {
+                $('.AnimaTion4').attr('src', './卡-img/桌機index_img/icon4-1.png')
+            }
+        )
+        $('.AnimaTion4').mouseout(
+            function () {
+                $('.AnimaTion4').attr('src', './卡-img/桌機index_img/icon4.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion5').mousemove(
+            function () {
+                $('.AnimaTion5').attr('src', './卡-img/桌機index_img/icon5-1.png')
+            }
+        )
+        $('.AnimaTion5').mouseout(
+            function () {
+                $('.AnimaTion5').attr('src', './卡-img/桌機index_img/icon5.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion6').mousemove(
+            function () {
+                $('.AnimaTion6').attr('src', './卡-img/桌機index_img/icon6-1.png')
+            }
+        )
+        $('.AnimaTion6').mouseout(
+            function () {
+                $('.AnimaTion6').attr('src', './卡-img/桌機index_img/icon6.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion7').mousemove(
+            function () {
+                $('.AnimaTion7').attr('src', './卡-img/桌機index_img/icon7-1.png')
+            }
+        )
+        $('.AnimaTion7').mouseout(
+            function () {
+                $('.AnimaTion7').attr('src', './卡-img/桌機index_img/icon7.png')
+            }
+        )
+        // ----------------------
+        $('.AnimaTion8').mousemove(
+            function () {
+                $('.AnimaTion8').attr('src', './卡-img/桌機index_img/icon8-1.png')
+            }
+        )
+        $('.AnimaTion8').mouseout(
+            function () {
+                $('.AnimaTion8').attr('src', './卡-img/桌機index_img/icon8.png')
+            }
+        )
